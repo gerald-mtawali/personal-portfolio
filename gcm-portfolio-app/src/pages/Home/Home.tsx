a70fd7155548e4a8e7ee60952457b79ade3140cd
gerald-mtawali <jeradi.mtawali@gmail.com>
2024-04-19 00:54:20 +0200

    Added changes to the Home Page. Included some new graphics as well.

--- gcm-portfolio-app/src/pages/Home/Home.tsx
+++ gcm-portfolio-app/src/pages/Home/Home.tsx
@@ -1,8 +1,24 @@
+import './Home.css'; 
+import '../../index.css';
+// import DevAvatar from '../../assets/image/DevAvatar.svg'; 
+import BrushStrokeBackground from '../../assets/image/brush-stroke-banner-background 2.svg'; 
+
 function HomePage() {
 	return (
-		<>
-			<h1> This is the Home Page! </h1>
-		</>
+		<div className='scroll'>
+			<div className='introContainer'>
+				<div id="intro_text" className='introDetail'>
+					<h2> My Name is Gerald Mtawali</h2>
+					<h3> Full Stack Software Engineer</h3>
+					<p> I design. I engineer. I create. </p>
+				</div>
+				<div className='introProfileContainer' id="intro_image">
+					<img src={BrushStrokeBackground} alt="Background" className='introProfileBackground'/>
+					{/* <img src={DevAvatar} alt="Avatar" className='introProfileForeground'/> */}
+				</div>
+			</div>
+			<hr className='solid'/>
+		</div>
 	);
 }
 
