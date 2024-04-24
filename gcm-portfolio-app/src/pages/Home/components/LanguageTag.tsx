aaacf90d2ca900f8d782b10d5b21dbe29b520fd9
gerald-mtawali <jeradi.mtawali@gmail.com>
2024-04-25 01:28:26 +0200

    Scaffolded Work Experience components

---
+++ gcm-portfolio-app/src/pages/Home/components/LanguageTag.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent } from "react"
+
+type props = {
+    language: string, 
+}
+
+export default function Tag({language}:props ){
+    const styles = {
+        tagContainer: {
+            backgroundColor: "var(--accent)", 
+            fontSize: "1rem",  
+            fontFamily: "var(--paragraph-font)", 
+            fontWeight: "medium", 
+        }
+    }
+
+    return (
+        <div className="tagContainer" style={styles.tagContainer}> 
+            <p>{language}</p>
+        </div>
+    ); 
+}
+
